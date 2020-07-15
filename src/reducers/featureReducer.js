7eb28ea1ac84f5fbed0ff0095887eee373dd1eba
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-15 12:23:07 -0600

    fixed total error

--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -36,7 +36,7 @@ export const featureReducer = (state = initialState, action) => {
             };
         case REMOVE_FEATURE:
             return {...state,
-                additionalPrice: state.additionalPrice + action.payload.price,
+                additionalPrice: state.additionalPrice - action.payload.price,
                 car: {
                 ...state.car,
                 features: [...state.car.features.filter(feature => {
